811bb339b9498237e321004ee154401257249a61
Suzan Hamza <suzan.hamza@gmail.com>
2022-01-04 21:00:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
