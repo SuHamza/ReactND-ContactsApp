d49c27061c3392b1b2ead6b9ec818ec8de405ec1
Suzan Hamza <suzan.hamza@gmail.com>
2022-01-04 21:12:05 +0200

    Hard Coded ContactList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: 'Tyler '},
+      { name: 'Karen' },
+      { name: 'Richard'}
+    ]
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <ContactList />
+      </div>
+    );
   }
 }
 
